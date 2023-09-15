1f60983fb836b0080ea80fcf0ecc4dce47b1cff0
Luis B <luis.beroiza@adesso.de>
2023-09-15 22:13:15 +0200

    Utiliza ReactiveDataDriverContextVariable
    Esta clase ayuda a controlar la contrapresion, en este caso se simuló un delay en el flujo para que agrupara una cierta cantidad de datos y los fuera mostrando de a poco.
    
    de otra manera Thymeleaf espera que el flujo termine para mostrarlo

--- src/main/java/com/lab/springboot/webflux/app/controllers/ProductController.java
+++ src/main/java/com/lab/springboot/webflux/app/controllers/ProductController.java
@@ -9,7 +9,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.thymeleaf.spring6.context.webflux.ReactiveDataDriverContextVariable;
 import reactor.core.publisher.Flux;
+
+import java.time.Duration;
 
 @Controller
 @RequestMapping("/product")
@@ -35,6 +38,33 @@ public class ProductController {
     productFlux.subscribe(product -> log.info("Show: " + product.getName()));
 
     model.addAttribute("productFlux", productFlux);
+    model.addAttribute("title", "List of Producs using Flux");
+    return "product/list";
+  }
+
+  // Cuando los datos tienen un Delay
+  // se pueden retornar usando un ReactiveDataDriverContextVariable
+  // Esta variable puede almacenar los tados en un buffer definido
+  // la cantidad de elementos
+  @GetMapping("/list-datadriver")
+  public String getProductListDataDriver(Model model) {
+    Flux<Product> productFlux = productService.findAll()
+        // se simula un delay en los datos
+        // cada dato del flujo necesita medio segundo de procesamiento
+        // antes de ser retornado
+        .delayElements(Duration.ofMillis(500));
+
+    productFlux.subscribe(product -> log.info("Show Data Driver: " + product.getName()));
+
+
+    // esta variable almacena un buffer de 1 elemento antes de retornarlo
+    // de esta forma la pagina se cargará de inmediato, pero irá
+    // mostrando los datos de a poco, de 2 en 2
+    // es decir la pagina tardará 1 segundo en mostrar grupos de 2 productos
+    // (cada producto tarda medio segundo y junta 2 antes de mostrarlos)
+    ReactiveDataDriverContextVariable dataDriver = new ReactiveDataDriverContextVariable(productFlux, 2);
+
+    model.addAttribute("productFlux", dataDriver);
     model.addAttribute("title", "List of Producs using Flux");
     return "product/list";
   }
